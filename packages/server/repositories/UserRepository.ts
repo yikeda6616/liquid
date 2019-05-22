574add2d90795a3c45f283dc5868424c03d8267b
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-21 23:24:39 -0700

    Add repositories

---
+++ packages/server/repositories/UserRepository.ts
@@ -0,0 +1,12 @@
+import { getRepository } from 'typeorm';
+import { User } from '../entities';
+
+export class UserRepository {
+  static async create(user: User) {
+    await getRepository(User).insert(user);
+  }
+
+  static async get(id: number) {
+    return await getRepository(User).findOne({ id });
+  }
+}
