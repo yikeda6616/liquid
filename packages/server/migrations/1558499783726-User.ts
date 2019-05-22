9770d313dfec62d8d11503fae25fbefa5204d904
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-21 23:24:15 -0700

    Add migrations

---
+++ packages/server/migrations/1558499783726-User.ts
@@ -0,0 +1,23 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class User1558499783726 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          { name: 'name', type: 'varchar' },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {}
+}
