edc9ced32fbb61bedbc48b63d60dcaacb05f362a
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-21 23:24:27 -0700

    Add services

---
+++ packages/server/services/UserService.ts
@@ -0,0 +1,12 @@
+import { UserRepository } from '../repositories/UserRepository';
+import { User } from '../entities/User';
+
+export class UserService {
+  static async create(user: User) {
+    await UserRepository.create(user);
+  }
+
+  static async get(id: number) {
+    return await UserRepository.get(id);
+  }
+}
