1c62e278ff4f09ace8edaab5024dffa79a83c640
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-21 23:24:02 -0700

    Add handlers

---
+++ packages/server/handlers/UserHandler.ts
@@ -0,0 +1,21 @@
+import { User } from '../entities';
+import { UserService } from '../services';
+import { Post, Get, Put, Delete } from '.';
+
+interface CreateRequest {
+  name: string;
+}
+
+class UserHandler {
+  @Post('/user')
+  static async create(params: CreateRequest) {
+    const user = new User();
+    user.name = params.name;
+    await UserService.create(user);
+  }
+
+  @Get('/user')
+  static async get() {
+    return await UserService.get(1);
+  }
+}
---
+++ packages/server/handlers/index.ts
@@ -0,0 +1,29 @@
+import * as express from 'express';
+
+export const handler = express();
+
+function createMethod(method: string) {
+  return (url: string) => (target, property) => {
+    handler[method](url, async (req, res) => {
+      // Merge query and body to one object
+      const params = {
+        ...req.params,
+        ...req.query,
+        ...req.body,
+      };
+      try {
+        const result = await target[property](params);
+        res.send(result || 'success');
+      } catch (err) {
+        res.status(400).send(err.message);
+      }
+    });
+  };
+}
+
+export const Get = createMethod('get');
+export const Post = createMethod('post');
+export const Put = createMethod('put');
+export const Delete = createMethod('delete');
+
+import './UserHandler';
