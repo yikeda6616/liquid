df13ee16bc133e51d8718c1d917ee9371e252e97
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-26 21:38:04 -0700

    Add insert

--- packages/server/repositories/ProductRepository.ts
+++ packages/server/repositories/ProductRepository.ts
@@ -2,7 +2,9 @@ import { Product } from '../entities';
 import { getRepository } from 'typeorm';
 
 export class ProductRepository {
-  static async create(product: Product) {}
+  static async create(product: Product) {
+    await getRepository(Product).insert(product);
+  }
 
   static async get(pid: number) {
     return await getRepository(Product).findOne(pid);
