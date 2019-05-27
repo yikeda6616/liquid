fca20dbf29c318456b218b957de1cdabaf341d0c
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-26 20:56:12 -0700

    Add columns

--- packages/server/entities/ProductCategory.ts
+++ packages/server/entities/ProductCategory.ts
@@ -1,11 +1,19 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class ProductCategory {
   @Column()
   pid: number;
+
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   createdAt: Date;
+
+  @Column()
   updatedAt: Date;
 }
