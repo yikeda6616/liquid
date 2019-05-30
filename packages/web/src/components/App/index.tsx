9a0fc4e844d71da8fe8d3722571513a63c7b7ee0
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-29 20:44:06 -0700

    Move google login button

--- packages/web/src/components/App/index.tsx
+++ packages/web/src/components/App/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { GoogleLogin } from 'react-google-login';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import axios from 'axios';
 
 import * as ROUTES from '../../constants/routes';
 import Navigation from '../Navigation';
@@ -10,22 +8,9 @@ import SignUp from '../SignUp';
 import SignIn from '../SignIn';
 import Admin from '../Admin';
 
-const responseGoogle = async (response: any) => {
-  console.log(response);
-  await axios.post('http://localhost:3030/api/login', response);
-};
-
 const App: React.FC = () => {
   return (
     <div className="App">
-      <GoogleLogin
-        responseType="code"
-        clientId="920424716394-smj5f8kk7mnke8dfkdiuspeo8qlgdfq5.apps.googleusercontent.com"
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-      />
       <Router>
         <Navigation />
         <hr />
--- packages/web/src/components/SignIn/index.tsx
+++ packages/web/src/components/SignIn/index.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import axios from 'axios';
+import { GoogleLogin } from 'react-google-login';
 
-const SignIn: React.FC = () => <h1>Sign In</h1>;
+const responseGoogle = async (response: any) => {
+  console.log(response);
+  await axios.post('http://localhost:3030/api/login', response);
+};
+
+const SignIn: React.FC = () => {
+  return (
+    <GoogleLogin
+      responseType="code"
+      clientId="920424716394-smj5f8kk7mnke8dfkdiuspeo8qlgdfq5.apps.googleusercontent.com"
+      buttonText="Login"
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      cookiePolicy={'single_host_origin'}
+    />
+  );
+};
 
 export default SignIn;
